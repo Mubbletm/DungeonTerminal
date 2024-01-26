91a4cdb3ff5f9cc45972cbeb590ef2892b33f2b9
Marbletm <noahtekent@gmail.com>
2024-01-26 13:26:01 +0100

    Packages

---
+++ DungeonTerminals/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace DungeonTerminals
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "DungeonTerminals";
+        public const string PLUGIN_NAME = "DungeonTerminals";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
